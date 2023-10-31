70ea080bd97ad7b4bec27bb7ced3439aa1f95b69
DEEPJYOTI DAS <95518873+Codessmasher@users.noreply.github.com>
2023-11-01 03:57:02 +0530

    Add files via upload

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,44 @@
+
+// css imported
+import "../Common.css"
+import "./Blog.css"
+
+
+function Blog() {
+    return (
+        <div className="Blogs ">
+
+            <div className="Blog_box box_shdow m_stndrd prjc" style={{ boxSizing: "border-box" }}>
+                <h2>Solution for not being able to copy PDF text because of encoded text</h2>
+                <p>I was Taking help from chatGPT studying with research paper but on a PDF when I copied the text it came like this “7XG0BVCRNJKHG6IUGIG6T7FFITFR” But I have solved it by implementing the following steps Go To ilovepdf Select PDF to Word Now Upload the encoded...</p>
+                <a href="https://codesplash.medium.com/solution-for-not-being-able-to-copy-pdf-text-because-of-encoded-text-bf25b4078272?source=user_profile---------0----------------------------">Read More</a>
+            </div>
+
+            <div className="Blog_box box_shdow m_stndrd prjc" style={{ boxSizing: "border-box" }}>
+                <h2>Next Greater Element-ii |Circularly find next greater number |Leetcode explanation</h2>
+                <p>As per the Aditya Verma stack playlist if a question has 2 for loop with O(n^2) solution and the 2nd for loop is dependent on first loop i then the question must have a better stack solution...</p>
+                <a href="https://codesplash.medium.com/next-greater-element-ii-circularly-find-next-greater-number-leetcode-explanation-c98fa33deee?source=user_profile---------1----------------------------">Read More</a>
+            </div>
+
+            <div className="Blog_box box_shdow m_stndrd prjc" style={{ boxSizing: "border-box" }}>
+                <h2>Get environment variable(dotenv)in both frontend and backend(MERN stack) with Vite</h2>
+                <p>If you are new to MERN stack development you have possibly heard about .env file which is usually used for the privacy of some important...</p>
+                <a href="https://codesplash.medium.com/get-environment-variable-dotenv-in-both-frontend-and-backend-mern-stack-with-vite-19c061e6dc19?source=user_profile---------2----------------------------">Read More</a>
+            </div>
+
+            <div className="Blog_box box_shdow m_stndrd prjc" style={{ boxSizing: "border-box" }}>
+                <h2>Module “buffer” has been externalized for browser compatibility. JWT error resolved</h2>
+                <p>I was working on a MERN stack project and I am new to the MERN stack. I am learning it through project building. While developing the project I have encountered this error and...</p>
+                <a href="https://codesplash.medium.com/module-buffer-has-been-externalized-for-browser-compatibility-jwt-error-resolved-dfd1e2e75867?source=user_profile---------3----------------------------">Read More</a>
+            </div>
+
+            <div className="Blog_box box_shdow m_stndrd prjc" style={{ boxSizing: "border-box" }}>
+                <h2>Search a 2D Matrix | Optimized explanation</h2>
+                <p>Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3 Output: true The elements are in increasing from (0,0) to (m-1,n-1). It means we can think of a Binary</p>
+                <a href="https://codesplash.medium.com/search-a-2d-matrix-optimized-explanation-261a91a397c0?source=user_profile---------4----------------------------">Read More</a>
+            </div>
+        </div>
+    )
+}
+
+export default Blog
