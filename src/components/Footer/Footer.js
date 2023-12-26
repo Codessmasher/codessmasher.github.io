b20df2ae58605946a5c3f332fbe28e9422f5685f
DEEPJYOTI DAS <95518873+Codessmasher@users.noreply.github.com>
2023-12-26 19:50:12 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,10 @@ import "./Footer.css"
 // imges
 import github from "../../images/github.svg"
 import linkedin from "../../images/linkedin.svg"
-import instagram from "../../images/instagram.png"
-// import codechef from "../../images/CodeChef.webp"
+import instagram from "../../images/instagram.png" 
 import leetcode from "../../images/leetcode.png"
+import gfg from "../../images/gfg.png"
+import medium from "../../images/medium.png"
 
 function Footer() {
   return (
@@ -16,16 +17,17 @@ function Footer() {
       <div className=" p_stndrd  ">
         <h3 className="txt_center p_stndrd">CONNECT WITH ME</h3>
         <ul className="skills flex_arnd m_stndrd">
-           <li><a href="https://github.com/Codessmasher"><img src={github} className="img_size_footer"/></a></li>
-           <li><a href="https://www.linkedin.com/in/CodessmasherDeepjyotiDas"><img src={linkedin} className="img_size_footer"/></a></li>
-           <li><a href="https://www.instagram.com/code._.smashers"><img src={instagram} className="img_size_footer"/></a></li>
-           {/* <li><a href="https://www.codechef.com/users/codesmashers13"><img src={codechef} className="img_size_footer"/></a></li> */}
-           <li><a href="https://leetcode.com/codesplash"><img src={leetcode} className="img_size_footer"/></a></li>
+           <li><a href="https://github.com/Codessmasher"><img src={github} alt='' className="img_size_footer"/></a></li>
+           <li><a href="https://www.linkedin.com/in/CodessmasherDeepjyotiDas"><img src={linkedin} alt='' className="img_size_footer"/></a></li>
+           <li><a href="https://www.instagram.com/code._.smashers"><img src={instagram} alt='' className="img_size_footer"/></a></li> 
+           <li><a href="https://leetcode.com/codesplash"><img src={leetcode} alt='' className="img_size_footer"/></a></li>
+           <li><a href="https://auth.geeksforgeeks.org/user/codesplash"><img src={gfg} alt='' className="img_size_footer"/></a></li>
+           <li><a href="https://codesplash.medium.com/"><img src={medium} alt='' className="img_size_footer"/></a></li>
         </ul>
       </div>
 
     </div>
-  )
+  ) 
 }
 
 export default Footer
